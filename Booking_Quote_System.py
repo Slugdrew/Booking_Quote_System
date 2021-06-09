6228eb0d1c4744456747da4a005a499aa66f770c
Slugdrew <andrewbh80@gmail.com>
2021-06-08 21:51:07 -0700

    Initial Commit

---
+++ Booking_Quote_System.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# ------------------------------------------#
+# Title: Booking_Quote_System.py
+# Desc: Assignment Booking Quote System - Create the Booking Quote application
+# Change Log: (Who, When, What)
+# AHernandez, 2021-June-08, Created File for Booking Quote application and skeletal structure 
+# ------------------------------------------#
---
+++ Test_Booking_Quote_System.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# ------------------------------------------#
+# Title: Test_Booking_Quote_System.py
+# Desc: Assignment Booking Quote System - Create the tests harness for the Booking Quote application
+# Change Log: (Who, When, What)
+# AHernandez, 2021-June-08, Created File
+# ------------------------------------------#
